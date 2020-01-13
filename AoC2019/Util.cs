30b9039838fb0a0036c75d6749c2ef7d8675ae6f
jjermanis <jermanis@yahoo.com>
2020-01-13 10:25:55 -0800

    Unit test for number utils

--- AoC2019/Util.cs
+++ AoC2019/Util.cs
@@ -6,19 +6,40 @@ namespace AoC2019
 {
     public class Util
     {
+        /// <summary>
+        /// Returns the smallest integer that is evenly divisible by all provided numbers
+        /// </summary>
         public static long LeastCommonMultiple(List<long> vals)
             => vals.Aggregate(LeastCommonMultiple);
+
+        /// <summary>
+        /// Returns the smallest integer that is evenly divisible by both numbers
+        /// </summary>
         public static long LeastCommonMultiple(long a, long b)
             => Math.Abs(a * b) / GreatestCommonDenom(a, b);
+
+        /// <summary>
+        /// Returns the largest integer that is a factor of both numbers
+        /// </summary>
         public static long GreatestCommonDenom(long a, long b)
             => b == 0 ? a : GreatestCommonDenom(b, a % b);
 
+        /// <summary>
+        /// Enumerates all possible permutations in the input.  This function does NOT
+        /// account for duplicates in the input.  For example, vals=[1, 1] will return
+        /// [1,1], [1,1]
+        /// </summary>
         public static IEnumerable<int[]> Permute(params int[] vals)
         {
             foreach (var val in PermuteWork(vals, 0, vals.Length-1))
                 yield return val;
         }
 
+        /// <summary>
+        /// Removes all common prime factors from x and y.  If x/y were a fraction,
+        /// the result of this function would be the reduced numerator and denominator
+        /// for that fraction.
+        /// </summary>
         public static (int x, int y) ReduceCommonFactors(int x, int y)
         {
             int resultX = x;
@@ -35,21 +56,33 @@ namespace AoC2019
             return (resultX, resultY);
         }
 
+        /// <summary>
+        /// Enumerates over all prime factors of val, in ascending order.  If a prime
+        /// factor is repeated, it will be repeated in the enumeration.  For example,
+        /// val=24 returns [2,2,2,3]
+        /// </summary>
         public static IEnumerable<int> PrimeFactors(int val)
         {
-            var remaining = val;
-            foreach (var prime in Primes())
+            if (val >= 2)
             {
-                while (remaining % prime == 0)
+                var remaining = val;
+                foreach (var prime in Primes())
                 {
-                    yield return prime;
-                    remaining /= prime;
+                    while (remaining % prime == 0)
+                    {
+                        yield return prime;
+                        remaining /= prime;
+                    }
+                    if (remaining == 1)
+                        break;
                 }
-                if (remaining == 1)
-                    break;
             }
         }
 
+        /// <summary>
+        /// Enumerates over all prime values. This does not stop; there is no
+        /// graceful handling for overflow.
+        /// </summary>
         public static IEnumerable<int> Primes()
         {
             yield return 2;
@@ -66,6 +99,9 @@ namespace AoC2019
             }
         }
 
+        /// <summary>
+        /// Returns true if and only if val is a prime number
+        /// </summary>
         public static bool IsPrime(int val)
         {
             if (val < 2)
---
+++ AocC2019Test/UtilTest.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AoC2019;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace AocC2019Test
+{
+    [TestClass]
+    public class UtilTest
+    {
+        [TestMethod]
+        public void TestIsPrime()
+        {
+            Assert.IsFalse(Util.IsPrime(-2));
+            Assert.IsFalse(Util.IsPrime(0));
+            Assert.IsFalse(Util.IsPrime(1));
+            Assert.IsTrue(Util.IsPrime(2));
+            Assert.IsTrue(Util.IsPrime(3));
+            Assert.IsFalse(Util.IsPrime(4));
+            Assert.IsTrue(Util.IsPrime(7919));
+            Assert.IsFalse(Util.IsPrime(947 * 953));
+        }
+
+        [TestMethod]
+        public void TestPrimesEnumerator()
+        {
+            var count = 0;
+            foreach (var prime in Util.Primes())
+            {
+                if (count++ > 100)
+                    break;
+                if (count == 100)
+                    Assert.AreEqual(541, prime);
+                Assert.IsTrue(Util.IsPrime(prime));
+            }
+        }
+
+        [TestMethod]
+        public void TestPrimeFactors()
+        {
+            Assert.AreEqual(0, Util.PrimeFactors(0).Count());
+            Assert.AreEqual(0, Util.PrimeFactors(1).Count());
+            Assert.IsTrue(Enumerable.SequenceEqual(new[] { 7 }, Util.PrimeFactors(7)));
+            Assert.IsTrue(Enumerable.SequenceEqual(new[] { 2, 2, 3 }, Util.PrimeFactors(12)));
+            Assert.IsTrue(Enumerable.SequenceEqual(new[] { 2, 2, 2, 2, 2, 2, 2, 2 }, Util.PrimeFactors(256)));
+            Assert.IsTrue(Enumerable.SequenceEqual(new[] { 2, 3, 5, 7, 11 }, Util.PrimeFactors(2310)));
+        }
+
+        [TestMethod]
+        public void TestReduceFactors()
+        {
+            Assert.AreEqual((1, 5), Util.ReduceCommonFactors(1, 5));
+            Assert.AreEqual((0, 5), Util.ReduceCommonFactors(0, 5));
+            Assert.AreEqual((2, 5), Util.ReduceCommonFactors(42, 105));
+            Assert.AreEqual((13, 19), Util.ReduceCommonFactors(13, 19));
+            Assert.AreEqual((8, 15), Util.ReduceCommonFactors(8, 15));
+            Assert.AreEqual((6, 35), Util.ReduceCommonFactors(66, 385));
+            Assert.AreEqual((14, 15), Util.ReduceCommonFactors(3961202, 4244145));
+        }
+
+        [TestMethod]
+        public void TestPermute()
+        {
+            // TODO Make better tests here
+            Assert.IsTrue(Util.Permute(3, 7, 13).ToList().Count == 6);
+            Assert.IsTrue(Util.Permute(3, 7, 13, 19).ToList().Count == 24);
+            Assert.IsTrue(Util.Permute(3, 7, 13, 19, 29).ToList().Count == 120);
+        }
+
+        [TestMethod]
+        public void TestCommonDenom()
+        {
+            Assert.AreEqual(1, Util.GreatestCommonDenom(1, 12));
+            Assert.AreEqual(2, Util.GreatestCommonDenom(0, 2));
+            Assert.AreEqual(2, Util.GreatestCommonDenom(2, 0));
+            Assert.AreEqual(1, Util.GreatestCommonDenom(14, 15));
+            Assert.AreEqual(4, Util.GreatestCommonDenom(4, 32));
+            Assert.AreEqual(1001, Util.GreatestCommonDenom(2002, 7007));
+        }
+
+        [TestMethod]
+        public void TestLeastCommonMultiple()
+        {
+            Assert.AreEqual(6, Util.LeastCommonMultiple(2, 3));
+            Assert.AreEqual(30, Util.LeastCommonMultiple(6, 15));
+            Assert.AreEqual(462, Util.LeastCommonMultiple(42, 66));
+
+            Assert.AreEqual(30, Util.LeastCommonMultiple(new List<long> { 2, 3, 5 }));
+            Assert.AreEqual(28_453_973_716, Util.LeastCommonMultiple(new List<long> { 206, 422, 1748, 2996}));
+        }
+    }
+}
